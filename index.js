9b15f0fe5feb185b71f8504b56805f3e8e602d99
AhshanHabib26 <habib.net27@gmail.com>
2023-02-06 02:22:40 +0600

    DB & Server Connect

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// MongoDB MiddleWare
+app.use(express.json());
+app.use(cors());
+
+// DB Connect
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("DB Connect Successfully".green.inverse);
+  })
+  .catch((err) => {
+    console.log(`${err}`.red.inverse);
+  });
 
 
+
+
+// Server Run
+app.listen(port, () => {
+  console.log("Server on Running".yellow.inverse);
+});
