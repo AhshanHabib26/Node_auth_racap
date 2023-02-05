43e0ec0d01478bf1f85813aeeca1c0ee8f1d7709
AhshanHabib26 <habib.net27@gmail.com>
2023-02-06 02:16:50 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
